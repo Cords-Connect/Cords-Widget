15cbe723c045629d4d7fedf9a9ddc1e187d98b44
Billy Hawkes <billyhawkes02@gmail.com>
2023-04-25 14:11:03 -0400

    Added build output as widget.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [solidPlugin()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+		rollupOptions: {
+			output: {
+				entryFileNames: "widget.js",
+			},
+		},
+	},
 });
