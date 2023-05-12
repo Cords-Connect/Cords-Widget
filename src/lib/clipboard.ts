172f4a4d5b97dec70e97fa09418f8580833654d0
Billy Hawkes <billyhawkes02@gmail.com>
2023-05-11 23:26:55 -0400

    Allowed cookies to be set on different sites

--- src/lib/clipboard.ts
+++ src/lib/clipboard.ts
@@ -9,14 +9,20 @@ export const saveService = (id: string) => {
 	if (clipboardIDs().includes(id)) {
 		return;
 	} else {
-		Cookies.set("clipboard", JSON.stringify([...clipboardIDs(), id]));
+		Cookies.set("clipboard", JSON.stringify([...clipboardIDs(), id]), {
+			sameSite: "None",
+			secure: true,
+		});
 		setClipboardIDs([...clipboardIDs(), id]);
 	}
 };
 
 export const removeService = (id: string) => {
 	const newClipboard = clipboardIDs().filter((service) => service !== id);
-	Cookies.set("clipboard", JSON.stringify(newClipboard));
+	Cookies.set("clipboard", JSON.stringify(newClipboard), {
+		sameSite: "None",
+		secure: true,
+	});
 	setClipboardIDs(newClipboard);
 };
 
@@ -25,7 +31,10 @@ export const isSaved = (id: string) => clipboardIDs().includes(id);
 export const initializeClipboard = () => {
 	const clipboardCookie = Cookies.get("clipboard");
 	if (!clipboardCookie) {
-		Cookies.set("clipboard", JSON.stringify([]));
+		Cookies.set("clipboard", JSON.stringify([]), {
+			sameSite: "None",
+			secure: true,
+		});
 	} else {
 		const services: unknown = JSON.parse(clipboardCookie);
 		const validServices = z.string().array().safeParse(services);
@@ -33,7 +42,10 @@ export const initializeClipboard = () => {
 			setClipboardIDs(validServices.data);
 		} else {
 			setClipboardIDs([]);
-			Cookies.set("clipboard", JSON.stringify([]));
+			Cookies.set("clipboard", JSON.stringify([]), {
+				sameSite: "None",
+				secure: true,
+			});
 		}
 	}
 };
