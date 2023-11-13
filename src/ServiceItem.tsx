b82d7dcd82f7fd29b3410c3dbe90f2bec27f5de9
Billy Hawkes <billyhawkes02@gmail.com>
2023-11-13 13:41:13 -0500

    Changed phone icon to be outline

--- src/ServiceItem.tsx
+++ src/ServiceItem.tsx
@@ -1,4 +1,5 @@
-import { FaRegularBookmark, FaRegularMap, FaSolidBookmark, FaSolidPhone } from "solid-icons/fa";
+import { FaRegularBookmark, FaRegularMap, FaSolidBookmark } from "solid-icons/fa";
+import { FiPhoneCall } from "solid-icons/fi";
 import { Component } from "solid-js";
 import { isSaved, removeService, saveService } from "./lib/clipboard";
 
@@ -35,7 +36,7 @@ const ServiceItem: Component<Props> = (props) => {
 					)}
 				</button>
 				<button class="flex gap-2 text-sm justify-center items-center border px-2.5 py-1.5 border-slate-200 rounded-full">
-					<FaSolidPhone size={15} class="text-gray-500" />
+					<FiPhoneCall size={16} class="text-gray-500" />
 					Call
 				</button>
 				<button class="flex gap-2 text-sm justify-center items-center border px-2.5 py-1.5 border-slate-200 rounded-full">
